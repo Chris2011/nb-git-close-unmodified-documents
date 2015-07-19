f8c12a03006a05c1600964fefad655f4075bca83
markiewb <benno.markiewicz@googlemail.com>
2015-07-19 20:29:18 +0200

    Find only the opened top components as candidates for closing #1

--- src/main/java/de/markiewb/netbeans/plugins/git/closeunmodifieddocuments/CloseUnmodifiedFilesAction.java
+++ src/main/java/de/markiewb/netbeans/plugins/git/closeunmodifieddocuments/CloseUnmodifiedFilesAction.java
@@ -233,7 +233,7 @@ public final class CloseUnmodifiedFilesAction implements ActionListener {
         final WindowManager wm = WindowManager.getDefault();
         for (Mode mode : wm.getModes()) {
             if (wm.isEditorMode(mode)) {
-                result.addAll(Arrays.asList(mode.getTopComponents()));
+                result.addAll(Arrays.asList(wm.getOpenedTopComponents(mode)));
             }
         }
         return result;
